2da67d1e7971f6120c594b753e46ee8099358410
Jonatan Retama <jonatanretama@gmail.com>
2023-02-15 20:13:18 -0600

    feat: mock data for people

--- libs/components/atoms/PeopleContent/people-content.tsx
+++ libs/components/atoms/PeopleContent/people-content.tsx
@@ -8,7 +8,6 @@ export type TPeopleContentProps = {
   planetId?: string;
 } & TPeopleDetailsArr;
 
-/* TODO: Update font styles */
 export const PeopleContent: FC<TPeopleContentProps> = ({
   planetName,
   keyName,
